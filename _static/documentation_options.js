3474723ba2bc895ec7f71aff0c5a04adb1ff8817
Marcelo Barboza <salve.barboza@pm.me>
2020-06-15 12:21:26 -0300

    mudancas importantes:
    1) adotei o tema 'shpinx_rtd_theme' por padrao;
    2) adotei o bibtex como gerenciador de referencias bibliograficas;
    3) adicionei uma secao que contem planos de ensino das disciplinas que
       estou lecionando;
    4) aterei o nome da pagina de 'bem vindo as minhas notas de aula' para
       'bem vindo a pagina do professor marcelo barboza', a fim de refletir
       o fato de que agora nao temos apenas notas de aula na pagina;
    TODO:
    1) acrescentar secoes acerca de LaTeX, GAP, Python, etc.;

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'a1',
+    VERSION: '0.1',
     LANGUAGE: 'pt_BR',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
