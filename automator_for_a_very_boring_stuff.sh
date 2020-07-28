7b2306d2fab5bbf797e8ff18ed78d6e466507fca
Marcelo Barboza <salve.barboza@pm.me>
2020-07-28 12:47:21 -0300

    made with <3

---
+++ automator_for_a_very_boring_stuff.sh
@@ -0,0 +1,13 @@
+#!/bin/ksh
+# automator_for_a_very_boring_stuff.sh
+
+SOURCE='~/projects/bitbucket/notas_de_aula/sphinx/build'
+DESTINATION='~/projects/github/marcelobarboza.github.io'
+
+cp "$SOURCE"/* "$DESTINATION"/
+
+git add .
+git commit -m "made with <3"
+git push
+
+echo "Done!"
